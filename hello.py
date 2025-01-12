bfff3cb17c32ccd43bb1fce90bfaf3a57b84e157
Fus <phuquach2005@gmail.com>
2025-01-12 21:17:30 +0700

    update

--- hello.py
+++ hello.py
@@ -3,8 +3,5 @@ print(1e-10)
 print(1.0e10)
 
 print (i for i in range(10))
-print (i for i in range(10) if i % 2 == 0)
-print (i for i in range(10) if i % 2 == 1)
-print (i for i in range(10) if i % 2 == 0 if i % 3 == 0)
+print([i for i in range(10)])
 
-print(1+1) #from namanhishere
